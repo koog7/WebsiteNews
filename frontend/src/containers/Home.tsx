56bb3316d650689966c78f7fb039808d895c3cd4
koog7 <k.ognev709@mail.com>
2024-08-24 12:28:01 +0600

    Сверстана главная страница с карточками новостей

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,7 +1,22 @@
+import {Button} from "@mui/material";
+import NewsCard from "../components/NewsCard.tsx";
+
 const Home = () => {
     return (
         <div>
-            123
+            <div>
+                <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+                    <div>
+                        <h1>Posts</h1>
+                    </div>
+                    <div>
+                        <Button variant="contained">Add new post</Button>
+                    </div>
+                </div>
+                <div>
+                    <NewsCard />
+                </div>
+            </div>
         </div>
     );
 };
