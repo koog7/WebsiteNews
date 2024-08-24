427cefcdc6c08579c0cf0ae6183fee7f21f0a070
koog7 <k.ognev709@mail.com>
2024-08-24 15:09:12 +0600

    Написан метод get для получения всех сообщений

--- frontend/src/containers/Thunk/FetchSlice.ts
+++ frontend/src/containers/Thunk/FetchSlice.ts
@@ -1,4 +1,6 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "../../app/store.ts";
+import axiosAPI from "../../axios/AxiosAPI.ts";
 
 interface News{
     id: string;
@@ -27,6 +29,20 @@ const initialState: NewsState = {
     error: false,
 };
 
+export const getNews = createAsyncThunk<News[], { state: RootState }>('news/getNews', async () => {
+    try{
+        const response = await axiosAPI.get<News[]>(`/news`);
+        return response.data.map(item => ({
+            id: item.id,
+            title: item.title,
+            image: item.image,
+            date: item.date,
+        }));
+    }catch (error) {
+        console.error('Error:', error);
+    }
+});
+
 export const NewsSlice = createSlice({
     name:'news',
     initialState,
@@ -34,6 +50,18 @@ export const NewsSlice = createSlice({
         setError: (state, action: PayloadAction<boolean>) => {
             state.error = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getNews.pending, (state: NewsState) => {
+            state.error = false;
+        }).addCase(getNews.fulfilled, (state: NewsState, action: PayloadAction<News[]>) => {
+            state.loading = false;
+            state.allNews = action.payload;
+            console.log(state.allNews)
+        }).addCase(getNews.rejected, (state: NewsState) => {
+            state.loading = false;
+            state.error = true;
+        });
     },
 })
 
